9b217f81f975892db6501426665b6bb6ef78a4e8
Alexandre Tea <alexandre.qtea@gmail.com>
2017-08-05 00:54:09 +0100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-C4.5 algorithm parameters
-- minimum leaf size
+# (DDTI) Distributed Decision Tree Induction
+TODO description
 
+## How to run
+TODO
+
+## Deployment
+TODO
+
+## Dependencies
+- An MPI implementation
+- Armadillo (for matrices)
+- MLpack (for Load() and Logger)
+
+## Misc
 Link to some datasets:
 https://ocw.mit.edu/courses/sloan-school-of-management/15-097-prediction-machine-learning-and-statistics-spring-2012/datasets/
--- srcs/utils/datetime.cpp
+++ srcs/utils/datetime.cpp
@@ -4,7 +4,7 @@
 // File:     /Users/alexandretea/Work/ddti/srcs/utils/datetime.cpp
 // Purpose:  TODO (a one-line explanation)
 // Created:  2017-07-29 22:52:26
-// Modified: 2017-07-29 22:56:44
+// Modified: 2017-08-05 00:45:41
 
 #include <ctime>
 #include "datetime.hpp"
@@ -15,8 +15,8 @@ namespace datetime {
 std::string
 now_str(char const* format)
 {
-    std::time_t		t = std::time(0);
-    char 			cstr[128];
+    std::time_t     t = std::time(0);
+    char            cstr[128];
 
     std::strftime(cstr, sizeof(cstr), format, std::localtime(&t));
     return cstr;
